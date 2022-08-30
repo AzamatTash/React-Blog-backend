cd41e1b40602884464a13b6a0b6c30c322c7b13d
Azamat <azamat300698@gmail.com>
2022-08-30 21:59:11 +0500

    #2 database connection

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+mongoose
+    .connect('mongodb+srv://admin:wwwwww@cluster0.mozteuu.mongodb.net/blog?retryWrites=true&w=majority')
+    .then(() => console.log('DB ok'))
+    .catch((err) => console.log('DB err', err))
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.listen(4444, (err) => {
     if(err) {
